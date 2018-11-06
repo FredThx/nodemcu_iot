e63f1bb57ab0fab90a1bb4ce2bba135235f361a3
Frédéric Thomé <fthome@pierron.fr>
2018-11-06 19:42:04 +0100

    autocompile

--- compile.lua
+++ compile.lua
@@ -1,42 +1,47 @@
-progs={ "init_mqtt",
-        "init_trig",
-        "main",
-        "params",
-        "read_and_send",
-        "telnet",
-        "add_reverse_topics",
-        "BMP_reader",
-        "ds18b20",
-        "ds1820_reader",
-        "DTH_reader",
-        "test_and_send",
-        "wifi",
-        "on_wifi_connected",
-        "433_switch",
-        "i2c_display",
-        "util",
-        "logger",
-        "i2c_lcd",
-        "i2c_geste",
-        "paj7620",
-        "mcp3008",
-        "maxsonar",
-        "servo",
-        "mpu6050",
-        "stepper",
-        "mcp3201",
-        "rotary",
-        "mcp9804",
-        "mcp9803",
-        }
-files = file.list()
-for k, prog in pairs(progs) do
-    if files[prog..".lua"] then
-        print(prog.."...")
-        file.remove(prog..".lc")
-        node.compile(prog..".lua")
-        file.remove(prog..".lua")
-        print("... done.")
+local nb
+do
+    progs={ "init_mqtt",
+            "init_trig",
+            "main",
+            "params",
+            "read_and_send",
+            "telnet",
+            "add_reverse_topics",
+            "BMP_reader",
+            "ds18b20",
+            "ds1820_reader",
+            "DTH_reader",
+            "test_and_send",
+            "wifi",
+            "on_wifi_connected",
+            "433_switch",
+            "i2c_display",
+            "util",
+            "logger",
+            "i2c_lcd",
+            "i2c_geste",
+            "paj7620",
+            "mcp3008",
+            "maxsonar",
+            "servo",
+            "mpu6050",
+            "stepper",
+            "mcp3201",
+            "rotary",
+            "mcp9804",
+            "mcp9803",
+            }
+    files = file.list()
+    for k, prog in pairs(progs) do
+        if files[prog..".lua"] then
+            nb = (nb or 0) + 1
+            print(prog.."...")
+            file.remove(prog..".lc")
+            node.compile(prog..".lua")
+            file.remove(prog..".lua")
+            print("... done.")
+        end
     end
+    files = nil
 end
-files = nil
+return nb
--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 print("Initialisation...")
 
 tmr.create():alarm(1000,tmr.ALARM_SINGLE, function ()
+        if dofile("compile.lua") then node.restart() end
         dofile("util.lc")
         _dofile("main")
     end)
