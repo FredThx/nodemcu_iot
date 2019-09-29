cb32dd6a1edda5449d535dd0a9e457d3a92cfb7f
FredThx <fredthx@gmail.com>
2019-09-29 18:09:05 +0200

    mise à jour vmc + servo (add auto_stop)

--- examples/vmc/params.lua
+++ examples/vmc/params.lua
@@ -2,7 +2,7 @@
 --  Projet : des IOT a base de nodemcu (ESP8266)
 --           qui communiquent en MQTT
 -------------------------------------------------
---  Auteur : FredThx  
+--  Auteur : FredThx
 -------------------------------------------------
 --  Ce fichier : paramètres pour nodemcu SERVO
 --               avec un servomoteur S1123
@@ -14,83 +14,63 @@
 --    bit, mqtt, pwm
 -------------------------------------------------
 
-LOGGER = false
-TELNET = false
-WATCHDOG = true
+local App = {}
 
--- MCP3008
---mcp = _dofile("mcp3008")
---mcp.init() -- default pins
---mcp.init = nil -- 952 bytes released
+do
 
-SERVO = _dofile('servo')
-SERVO.init(1)
+    App.watchdog = {timeout = 30*60} -- set false or nil 30*60 = 30 minutes
+    App.msg_debug = true -- if true : send messages (ex : "MQTT send : ok")
 
--- Relay pour piloter petite vitesse (LOW) et grande vitesse (HIGH)
-RELAY_PIN = 4
-gpio.mode(RELAY_PIN, gpio.OUTPUT)
-gpio.write(RELAY_PIN, gpio.LOW)
-------------------------------
--- Modules a charger
-------------------------------
-modules={}
+    Servo = require('servo')
+    SERVO=Servo(1,1000)
 
-------------------
--- Params WIFI 
-------------------
-SSID = {"WIFI_THOME1","WIFI_THOME2"}
-PASSWORD = "plus33324333562"
-HOST = "NODE-VMC"
-wifi_time_retry = 10 -- minutes
+    -- Relay pour piloter petite vitesse (LOW) et grande vitesse (HIGH)
+    RELAY_PIN = 4
+    gpio.mode(RELAY_PIN, gpio.OUTPUT)
+    gpio.write(RELAY_PIN, gpio.LOW)
 
---------------------
--- Params MQTT
---------------------
-mqtt_host = "192.168.10.155"
-mqtt_port = 1883
-mqtt_user = "fredthx"
-mqtt_pass = "GaZoBu"
-mqtt_client_name = HOST
-mqtt_base_topic = "T-HOME/VMC/"
+  ------------------
+  -- Params WIFI
+  ------------------
+  App.net = {
+          ssid = {"WIFI_THOME1",'WIFI_THOME2'},
+          password = "plus33324333562",
+          wifi_time_retry = 10, -- minutes
+          }
 
--- Messages MQTT sortants
-mesure_period = 1*60 * 1000
-mqtt_out_topics = {}
-mqtt_out_topics[mqtt_base_topic.."_WATCHDOG"]={
-                message = "INIT",
-                qos = 0, retain = 0, callback = nil}
--- Messages sur trigger GPIO
-mqtt_trig_topics = {}
+  --------------------
+  -- Params MQTT
+  --------------------
+  App.mqtt = {
+      host = "192.168.10.155",
+      port = 1883,
+      user = "fredthx",
+      pass = "GaZoBu",
+      client_name = "NODE-VMC",
+      base_topic = "T-HOME/VMC/"
+  }
+    -- Actions sur messages MQTT entrants
+    App.mqtt_in_topics = {}
+    App.mqtt_in_topics[App.mqtt.base_topic.."WC/angle"]=function(data)
+                    SERVO.angle(tonumber(data))
+                end
+    App.mqtt_in_topics[App.mqtt.base_topic.."WC"]={
+                ["ON"]=function()
+                            SERVO:set_angle(90)
+                        end,
+                ["OFF"]=function()
+                            SERVO:set_angle(0)
+                        end}
 
--- Actions sur messages MQTT entrants
-mqtt_in_topics = {}
-mqtt_in_topics[mqtt_base_topic.."WC/angle"]=function(data)
-                SERVO.angle(tonumber(data))
-            end
-mqtt_in_topics[mqtt_base_topic.."WC"]={
-            ["ON"]=function()
-                        SERVO.angle(90)
-                    end,
-            ["OFF"]=function()
-                        SERVO.angle(0)
-                    end}
-					
-mqtt_in_topics[mqtt_base_topic.."vitesse"]={
-            ["HIGH"]=function()
-                      print("Vitesse : HIGH.")
-                        gpio.write(RELAY_PIN,gpio.HIGH)
-                    end,
-            ["LOW"]=function()
-                        print("Vitesse : LOW.")
-                        gpio.write(RELAY_PIN,gpio.LOW)
-                    end}
-					
--- Messages MQTT sortants sur test
-test_period = 500
---test_etat_porte = false
-mqtt_test_topics = {}
+    App.mqtt_in_topics[App.mqtt.base_topic.."vitesse"]={
+                ["HIGH"]=function()
+                          print("Vitesse : HIGH.")
+                            gpio.write(RELAY_PIN,gpio.HIGH)
+                        end,
+                ["LOW"]=function()
+                            print("Vitesse : LOW.")
+                            gpio.write(RELAY_PIN,gpio.LOW)
+                        end}
 
---Gestion du display : mqtt(json)=>affichage
-disp_texts = {}
-
-          
+end
+return App
--- servo.lua
+++ servo.lua
@@ -1,13 +1,13 @@
 -- servomotor module
 --
 -- usage :
---  Servo = require('servo_')
---  servo_1 = Servo(5) -- pin = 5
+--  Servo = require('servo')
+--  servo_1 = Servo(5, 1000) -- pin = 5, auto stop after 1000 milliseconds (factultatif)
 --  servo_1:start()
 --  servo_1:set_angle(angle) angle entre 0 et 156
 --  servo_1:stop()
--- memory usage : 2240 octets (if compiled)
---                + 104 octets per instance
+-- memory usage : 3248 octets (if compiled)
+--                + 304 octets per instance
 
 local Servo = {}
 Servo.__index = Servo
@@ -18,11 +18,15 @@ setmetatable(Servo, {
   end,
 })
 
-function Servo.new(pin)
+function Servo.new(pin, timer_stop)
   print("Create new Servo with pin = " .. pin)
   local self = setmetatable({}, Servo)
   self.pin = pin
   pwm.setup(pin,50,25)
+  if timer_stop then
+    self.timer_stop = timer_stop
+    self.alarm = tmr.create()
+  end
   return self
 end
 
@@ -37,6 +41,9 @@ end
 function Servo:set_angle(angle)
     if angle >= 0 and angle <= 200 then
         pwm.setduty(self.pin, 25+angle*5/9)
+    end
+    if self.alarm then
+      self.alarm:alarm(self.timer_stop, tmr.ALARM_SINGLE, function() self:stop() end)
     end
 end
 
